44eecf990757d3053367e25ec6931a5644876a9c
Дарья <167187299+Kaishivee@users.noreply.github.com>
2024-08-03 11:30:56 +0300

    Интроспекция.py

--- Интроспекция.py
+++ Интроспекция.py
@@ -7,7 +7,7 @@ def introspection_info(x):
     info = {
         'type': type(x),
         'attributes': [i for i in dir(x) if i.startswith('_')],
-        'methods': [i for i in dir(x) if not i.startswith('_')],
+        'methods': [n for n in dir(x) if not i.startswith('_')],
         'module': type(x).__module__
     }
     return info
